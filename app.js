95344c117dc14861dab221fe4390cbf8463752a0
Alejandro Vazquez <alex@alejandros-imac.home>
2017-12-15 08:43:59 -0800

    Added weather file and api.json file

---
+++ app.js
@@ -0,0 +1,3 @@
+const weather = require('./weather.js')
+//Join multiple values passed as arguments and replace all spaces with underscores
+// const query = process.argx.slice(2).join("_").replace(' ', '_');
